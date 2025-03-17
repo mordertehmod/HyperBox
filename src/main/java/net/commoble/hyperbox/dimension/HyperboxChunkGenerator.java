97024b18e60ea3f62771efaec2cc4d8e25f57be7
mordertehmod <brycemcc5433@gmail.com>
2025-03-17 08:00:23 -0500

    Fix registering of heightmaps

--- src/main/java/net/commoble/hyperbox/dimension/HyperboxChunkGenerator.java
+++ src/main/java/net/commoble/hyperbox/dimension/HyperboxChunkGenerator.java
@@ -27,10 +27,13 @@ import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.biome.FixedBiomeSource;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.Heightmap.Types;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.blending.Blender;
@@ -47,17 +50,17 @@ public class HyperboxChunkGenerator extends ChunkGenerator
 	public static final BlockPos MAX_SPAWN_CORNER = HyperboxChunkGenerator.CORNER.offset(80,1,80);
 
 	private final Holder<Biome> biome; public Holder<Biome> biome() { return biome; }
-		
+
 	/** get from Hyperbox.INSTANCE.hyperboxChunkGeneratorCodec.get(); **/
 	public static MapCodec<HyperboxChunkGenerator> makeCodec()
 	{
 		return Biome.CODEC.fieldOf("biome")
-			.xmap(HyperboxChunkGenerator::new, HyperboxChunkGenerator::biome);
+				.xmap(HyperboxChunkGenerator::new, HyperboxChunkGenerator::biome);
 	}
-	
-	// hardcoding this for now, may reconsider later
-	public int getHeight() { return 127; }
-	
+
+	// Updated to match dimension definition
+	public int getHeight() { return 128; }
+
 	// create chunk generator at runtime when dynamic dimension is created
 	public HyperboxChunkGenerator(MinecraftServer server)
 	{
@@ -77,7 +80,7 @@ public class HyperboxChunkGenerator extends ChunkGenerator
 	{
 		return Hyperbox.INSTANCE.hyperboxChunkGeneratorCodec.get();
 	}
-	
+
 	// apply carvers
 	@Override
 	public void applyCarvers(WorldGenRegion world, long seed, RandomState random, BiomeManager biomeManager, StructureManager structureManager, ChunkAccess chunkAccess, GenerationStep.Carving carvingStep)
@@ -93,64 +96,102 @@ public class HyperboxChunkGenerator extends ChunkGenerator
 		int chunkX = chunkPos.x;
 		int chunkZ = chunkPos.z;
 
+		// Pre-initialize heightmaps for this chunk
+		for (Heightmap.Types type : Heightmap.Types.values()) {
+			chunk.getOrCreateHeightmapUnprimed(type);
+		}
+
 		if (chunkX >= 0 && chunkX < 10 && chunkZ >= 0 && chunkZ < 10) {
 			BlockState wallState = Hyperbox.INSTANCE.hyperboxWall.get().defaultBlockState();
 			BlockPos.MutableBlockPos mutaPos = new BlockPos.MutableBlockPos();
 
 			int startX = chunkX * 16; // World X position of this chunk
 			int startZ = chunkZ * 16; // World Z position of this chunk
-			int endX = startX + 17;
-			int endZ = startZ + 17;
+			int endX = startX + 16;
+			int endZ = startZ + 16;
 
 			int maxHorizontal = 160;  // Box size
 			int ceilingY = this.getHeight() - 1;
 
+			// Fill each block in the chunk
 			for (int worldX = startX; worldX < endX; worldX++) {
 				for (int worldZ = startZ; worldZ < endZ; worldZ++) {
-					// Check if this block is within the walls
-					if (worldX == 0 || worldX == maxHorizontal - 1 || worldZ == 0 || worldZ == maxHorizontal - 1) {
+					// Process each column
+					boolean isWall = (worldX == 0 || worldX == maxHorizontal - 1 || worldZ == 0 || worldZ == maxHorizontal - 1);
+
+					// Set floor
+					mutaPos.set(worldX, 0, worldZ);
+					chunk.setBlockState(mutaPos, wallState, false);
+
+					// Set walls
+					if (isWall) {
 						for (int y = 1; y < ceilingY; y++) {
 							mutaPos.set(worldX, y, worldZ);
 							chunk.setBlockState(mutaPos, wallState, false);
 						}
+					} else {
+						for (int y = 1; y < ceilingY; y++) {
+							mutaPos.set(worldX, y, worldZ);
+							chunk.setBlockState(mutaPos, Blocks.AIR.defaultBlockState(), false);
+						}
 					}
-					// Generate floor and ceiling
-					mutaPos.set(worldX, 0, worldZ);
-					chunk.setBlockState(mutaPos, wallState, false);
+
+					// Set ceiling
 					mutaPos.set(worldX, ceilingY, worldZ);
 					chunk.setBlockState(mutaPos, wallState, false);
+
+					// Update heightmaps manually for this column
+					for (Heightmap.Types type : Heightmap.Types.values()) {
+						Heightmap heightmap = chunk.getOrCreateHeightmapUnprimed(type);
+						heightmap.update(worldX & 15, ceilingY, worldZ & 15, wallState);
+					}
 				}
 			}
 
-			BlockState aperture = Hyperbox.INSTANCE.apertureBlock.get().defaultBlockState();
-			Consumer<Direction> apertureSetter = dir -> chunk.setBlockState(mutaPos, aperture.setValue(ApertureBlock.FACING, dir), false);
+			// Only place apertures if this is in the correct chunk
+			// Calculate CENTER chunk coordinates
+			int centerChunkX = CENTER.getX() >> 4; // Divide by 16
+			int centerChunkZ = CENTER.getZ() >> 4; // Divide by 16
 
-			int centerX = CENTER.getX();
-			int centerY = CENTER.getY();
-			int centerZ = CENTER.getZ();
+			if (chunkX == centerChunkX && chunkZ == centerChunkZ) {
+				BlockState aperture = Hyperbox.INSTANCE.apertureBlock.get().defaultBlockState();
+				Consumer<Direction> apertureSetter = dir -> {
+					chunk.setBlockState(mutaPos, aperture.setValue(ApertureBlock.FACING, dir), false);
 
-			int west = centerX - 7;
-			int east = centerX + 7;
-			int down = centerY - 7;
-			int up = centerY + 7;
-			int north = centerZ - 7;
-			int south = centerZ + 7;
+					// Update heightmaps for the aperture position
+					for (Heightmap.Types type : Heightmap.Types.values()) {
+						Heightmap heightmap = chunk.getOrCreateHeightmapUnprimed(type);
+						heightmap.update(mutaPos.getX() & 15, mutaPos.getY(), mutaPos.getZ() & 15, aperture);
+					}
+				};
 
-			mutaPos.set(centerX,up,centerZ);
-			apertureSetter.accept(Direction.DOWN);
-			mutaPos.set(centerX,down,centerZ);
-			apertureSetter.accept(Direction.UP);
-			mutaPos.set(centerX,centerY,south);
-			apertureSetter.accept(Direction.NORTH);
-			mutaPos.set(centerX,centerY,north);
-			apertureSetter.accept(Direction.SOUTH);
-			mutaPos.set(east,centerY,centerZ);
-			apertureSetter.accept(Direction.WEST);
-			mutaPos.set(west,centerY,centerZ);
-			apertureSetter.accept(Direction.EAST);
+				int centerX = CENTER.getX();
+				int centerY = CENTER.getY();
+				int centerZ = CENTER.getZ();
+
+				int west = centerX - 7;
+				int east = centerX + 7;
+				int down = centerY - 7;
+				int up = centerY + 7;
+				int north = centerZ - 7;
+				int south = centerZ + 7;
+
+				mutaPos.set(centerX, up, centerZ);
+				apertureSetter.accept(Direction.DOWN);
+				mutaPos.set(centerX, down, centerZ);
+				apertureSetter.accept(Direction.UP);
+				mutaPos.set(centerX, centerY, south);
+				apertureSetter.accept(Direction.NORTH);
+				mutaPos.set(centerX, centerY, north);
+				apertureSetter.accept(Direction.SOUTH);
+				mutaPos.set(east, centerY, centerZ);
+				apertureSetter.accept(Direction.WEST);
+				mutaPos.set(west, centerY, centerZ);
+				apertureSetter.accept(Direction.EAST);
+			}
 		}
 	}
-	
+
 	@Override
 	public void spawnOriginalMobs(WorldGenRegion region)
 	{
@@ -166,56 +207,81 @@ public class HyperboxChunkGenerator extends ChunkGenerator
 	@Override
 	public CompletableFuture<ChunkAccess> fillFromNoise(Blender blender, RandomState random, StructureManager structures, ChunkAccess chunk)
 	{
-		// this is where the flat chunk generator generates flat chunks
+		// Initialize all heightmaps without using section.unlock()
+		for (Heightmap.Types type : Heightmap.Types.values()) {
+			chunk.getOrCreateHeightmapUnprimed(type);
+		}
+
 		return CompletableFuture.completedFuture(chunk);
 	}
 
 	@Override
 	public int getSeaLevel()
 	{
-		// only used by features' generate methods
 		return 0;
 	}
 
 	@Override
 	public int getMinY()
 	{
-		// the lowest y-level in the dimension
-		// debug -> 0
-		// flat -> 0
-		// noise -> NoiseSettings#minY
-			// overworld -> -64
-			// nether -> 0
 		return 0;
 	}
 
 	@Override
 	public int getBaseHeight(int x, int z, Types heightmapType, LevelHeightAccessor level, RandomState random)
 	{
-		// flat chunk generator counts the solid blockstates in its list
-		// debug chunk generator returns 0
-		// the "normal" chunk generator generates a height via noise
-		// we can assume that this is what is used to define the "initial" heightmap
-		return 0;
+		// Calculate the heightmap value at the specified position
+		boolean isWall = (x == 0 || x == 159 || z == 0 || z == 159);
+
+		switch (heightmapType) {
+			case WORLD_SURFACE:
+			case WORLD_SURFACE_WG:
+				return this.getHeight() - 1; // Ceiling height
+			case OCEAN_FLOOR:
+			case OCEAN_FLOOR_WG:
+				return 0; // Floor is at y=0
+			case MOTION_BLOCKING:
+			case MOTION_BLOCKING_NO_LEAVES:
+				return isWall ? this.getHeight() - 1 : 0;
+			default:
+				return 0;
+		}
 	}
 
-	// get base column
 	@Override
 	public NoiseColumn getBaseColumn(int x, int z, LevelHeightAccessor level, RandomState random)
 	{
-		// flat chunk generator returns a reader over its blockstate list
-		// debug chunk generator returns a reader over an empty array
-		// normal chunk generator returns a column whose contents are either default block, default fluid, or air
-		
-		return new NoiseColumn(0, new BlockState[0]);
+		BlockState wallState = Hyperbox.INSTANCE.hyperboxWall.get().defaultBlockState();
+
+		// Check if position is at the wall
+		boolean isWall = (x == 0 || x == 159 || z == 0 || z == 159);
+
+		// Generate a column of blocks
+		BlockState[] states = new BlockState[this.getHeight()];
+
+		// Fill the column appropriately
+		states[0] = wallState; // Floor
+
+		// Walls or empty space
+		for (int y = 1; y < this.getHeight() - 1; y++) {
+			states[y] = isWall ? wallState : Blocks.AIR.defaultBlockState();
+		}
+
+		// Ceiling
+		states[this.getHeight() - 1] = wallState;
+
+		return new NoiseColumn(0, states);
 	}
 
 	@Override
 	public void addDebugScreenInfo(List<String> stringsToRender, RandomState random, BlockPos pos)
 	{
-		// no info to add
+		// Add hyperbox debug info
+		stringsToRender.add("Hyperbox Dimension");
+		stringsToRender.add("Height: " + this.getHeight());
+		stringsToRender.add("MinY: " + this.getMinY());
 	}
-	
+
 	// let's make sure some of the default chunk generator methods aren't doing
 	// anything we don't want them to either
 
@@ -226,24 +292,24 @@ public class HyperboxChunkGenerator extends ChunkGenerator
 	{
 		return null;
 	}
-	
+
 	// decorate biomes with features
 	@Override
 	public void applyBiomeDecoration(WorldGenLevel world, ChunkAccess chunkAccess, StructureManager structures)
 	{
 		// noop
 	}
-	
+
 	@Override
 	public int getSpawnHeight(LevelHeightAccessor level)
 	{
 		return 1;
 	}
-	
+
 	// create structure references
 	@Override
 	public void createReferences(WorldGenLevel world, StructureManager structures, ChunkAccess chunk)
 	{
 		// no structures
-	}	
+	}
 }
